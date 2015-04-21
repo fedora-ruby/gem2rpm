a35ace1586ac03a1c50a0ac74d3473ec498809d3
Vít Ondruch <vondruch@redhat.com>
2015-04-21 12:35:11 +0200

    Add Templates::list.

--- lib/gem2rpm/templates.rb
+++ lib/gem2rpm/templates.rb
@@ -3,5 +3,11 @@ module Gem2Rpm
     def self.location
       @@location ||= File.join(File.dirname(__FILE__), '..', '..', 'templates')
     end
+
+    def self.list
+      @@list ||= Dir.chdir(location) do
+        Dir.glob('*').sort
+      end
+    end
   end
 end
