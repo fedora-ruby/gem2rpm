481758b6153647af3f156f2c9ac72bdaa0710c7e
Jun Aruga <jaruga@redhat.com>
2016-05-13 18:22:47 +0200

    Remove exit method from files in lib. https://github.com/fedora-ruby/gem2rpm/issues/74

--- test/test_configuration.rb
+++ test/test_configuration.rb
@@ -12,7 +12,34 @@ class TestConfiguration < Minitest::Test
   end
 
   def test_options
+    setup_options
     assert @config.options
     assert_includes @config.options, :args
+  end
+
+  def test_option_templates
+    setup_options('--templates')
+    assert(@config.options)
+    assert_includes(@config.options, :templates)
+  end
+
+  def test_option_version
+    setup_options('-v')
+    assert(@config.options)
+    assert_includes(@config.options, :version)
+  end
+
+  def test_invalid_option
+    e = assert_raises(Gem2Rpm::Configuration::InvalidOption) do
+      setup_options('-Z')
+    end
+    assert_match(/\Ainvalid option: -Z\n\nUsage:/m, e.to_s)
+  end
+
+  private
+
+  def setup_options(string = '')
+    argv = string.split
+    @config.send(:handle_options, argv)
   end
 end
