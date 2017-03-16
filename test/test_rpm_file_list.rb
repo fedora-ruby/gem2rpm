56c2800df9b528a19a021283175642635b165f9f
Vít Ondruch <vondruch@redhat.com>
2017-03-16 10:39:14 +0100

    Refactor RpmFileList tests.

--- test/test_rpm_file_list.rb
+++ test/test_rpm_file_list.rb
@@ -2,7 +2,11 @@ require 'helper'
 require 'gem2rpm/rpm_file_list'
 
 class TestRpmFileList < Minitest::Test
-  def file_list
+  def setup
+    @file_list = Gem2Rpm::RpmFileList.new(files)
+  end
+
+  def files
     %w(
       lib/foo.rb
       lib/bar.rb
@@ -14,26 +18,25 @@ class TestRpmFileList < Minitest::Test
   end
 
   def test_converts_to_rpm_files
-    Gem2Rpm::RpmFileList.new(file_list).entries.each do |e|
+    @file_list.entries.each do |e|
       assert_instance_of Gem2Rpm::RpmFile, e
     end
   end
 
   def test_top_level_entries
     assert_equal ['lib', 'LICENSE', '.gitignore', 'AUTHORS'],
-      Gem2Rpm::RpmFileList.new(file_list).top_level_entries.to_a
+      @file_list.top_level_entries.to_a
   end
 
   def test_to_rpm
     results =
-      "#{config.macro_for(:instdir)}/#{file_list[0]}\n" \
-      "#{config.macro_for(:instdir)}/#{file_list[1]}\n" \
-      "#{config.macro_for(:instdir)}/#{file_list[2]}\n" \
-      "#{config.macro_for(:license)} #{config.macro_for(:instdir)}/#{file_list[3]}\n" \
-      "#{config.macro_for(:ignore)} #{config.macro_for(:instdir)}/#{file_list[4]}\n" \
-      "#{config.macro_for(:doc)} #{config.macro_for(:instdir)}/#{file_list[5]}"
+      "#{config.macro_for(:instdir)}/#{files[0]}\n" \
+      "#{config.macro_for(:instdir)}/#{files[1]}\n" \
+      "#{config.macro_for(:instdir)}/#{files[2]}\n" \
+      "#{config.macro_for(:license)} #{config.macro_for(:instdir)}/#{files[3]}\n" \
+      "#{config.macro_for(:ignore)} #{config.macro_for(:instdir)}/#{files[4]}\n" \
+      "#{config.macro_for(:doc)} #{config.macro_for(:instdir)}/#{files[5]}"
 
-    assert_equal results, Gem2Rpm::RpmFileList.new(file_list).to_rpm
+    assert_equal results, @file_list.to_rpm
   end
-
 end
