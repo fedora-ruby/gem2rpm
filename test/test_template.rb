82fabb6cb930c0c4806758f9e318a3ab79021325
Vít Ondruch <vondruch@redhat.com>
2025-12-09 16:22:05 +0100

    Add SPDX support for F38+
    Fixes #113

--- test/test_template.rb
+++ test/test_template.rb
@@ -15,8 +15,8 @@ class TestTemplate < Minitest::Test
         fedora-21-25.spec.erb
         fedora-26-vagrant-plugin.spec.erb
         fedora-26.spec.erb
-        fedora-27-rawhide-vagrant-plugin.spec.erb
-        fedora-27-rawhide.spec.erb
+        fedora-38-rawhide-vagrant-plugin.spec.erb
+        fedora-38-rawhide.spec.erb
         fedora.spec.erb
         opensuse.spec.erb
         pld.spec.erb
